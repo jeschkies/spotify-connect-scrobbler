ccb222e0a4afb88f378b3bcb54d92b480aa1403d
Karsten Jeschkies <jeskar@web.de>
2017-05-31 22:30:34 +0200

    Define entry point for package.

--- setup.py
+++ setup.py
@@ -2,5 +2,11 @@ from setuptools import setup
 
 setup(
     name='spotify_connect_scrobbler',
-    packages=['spotify_connect_scrobbler']
+    version='0.1',
+    license='MIT',
+    packages=['spotify_connect_scrobbler'],
+    install_requires=['python-dateutil', 'requests'],
+    entry_points = {
+        'console_scripts': ['scrobbler=spotify_connect_scrobbler.scrobbler:main']
+    }
     )
