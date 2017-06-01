b806d5c98171ea0093f40acdeb13d0d66b235cdc
Karsten Jeschkies <jeskar@web.de>
2017-06-01 21:52:45 +0200

    Renew token if it expired.

--- spotify_connect_scrobbler/spotify.py
+++ spotify_connect_scrobbler/spotify.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import base64
 import os
 import requests
 import secrets
@@ -8,13 +9,29 @@ import urllib
 class SpotifyClient:
     """ A simple client for the Spotify Web API."""
 
+    def __init__(self, client_id, client_secret):
+        """Create Spotify client.
+
+        Args:
+            client_id (str): Identifies the client and the app.
+            client_secret (str): API secret.
+        """
+        self.__client_id = client_id
+        self.__client_secret = client_secret
+
+    def _make_authorization_headers(self):
+        auth = "{}:{}".format(self.__client_id, self.__client_secret).encode('ascii')
+        auth_base64 = base64.b64encode(auth).decode('ascii')
+        return {'Authorization': "Basic {}".format(auth_base64)}
+
+
     def request_authorization(self):
         """Returns an URL the user has to follow to authorize this app."""
 
         # Requests authorization
         request_secret = secrets.token_hex()
         payload = {
-            'client_id': SPOTIFY_CLIENT_ID,
+            'client_id': self.__client_id,
             'response_type': 'code',
             'redirect_uri': 'https://localhost:4000/steps/2',
             'scope': 'user-read-recently-played',
@@ -41,12 +58,33 @@ class SpotifyClient:
             'grant_type': 'authorization_code',
             'code': code,
             'redirect_uri': 'https://localhost:4000/steps/2',
-            'client_id': SPOTIFY_CLIENT_ID,
-            'client_secret': SPOTIFY_CLIENT_SECRET
+            'client_id': self.__client_id,
+            'client_secret': self.__client_secret
         }
         return requests.post(
                 'https://accounts.spotify.com/api/token', data=payload).json()
 
+
+    def refresh_access_token(self, refresh_token):
+        """ Returns the access token for Spotify Web API using a refresh token.
+
+        Args:
+            refresh_token (string): The token has been returned by the access
+            token request.
+
+        Returns:
+            dict: The response from Spotify.
+        """
+        # Get new auth token
+        payload = {
+            'grant_type': 'refresh_token',
+            'refresh_token': refresh_token
+        }
+        headers = self._make_authorization_headers()
+        return requests.post(
+                'https://accounts.spotify.com/api/token',
+                data=payload,
+                headers=headers).json()
 
     def recently_played_tracks(self, auth):
         """Query Spotify for the recently played tracks of user.
@@ -64,7 +102,13 @@ class SpotifyClient:
                 'https://api.spotify.com/v1/me/player/recently-played?limit=50',
                 data=payload, headers=headers)
         if response.ok:
-            return response
+            return response.json()
+        elif response.status_code == 401:
+            print("Spotify access token expired")
+            auth = self.refresh_access_token(auth['refresh_token'])
+            print(auth)
+            # Retry
+            return self.recently_played_tracks(auth)
         else:
             print(response.text)
             sys.exit(1)
@@ -74,7 +118,7 @@ if __name__ == "__main__":
     SPOTIFY_CLIENT_ID = os.environ['SPOTIFY_CLIENT_ID']
     SPOTIFY_CLIENT_SECRET = os.environ['SPOTIFY_CLIENT_SECRET']
 
-    client = SpotifyClient()
+    client = SpotifyClient(SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET)
     auth_url = client.request_authorization()
     print('Go to:')
     print(auth_url)
